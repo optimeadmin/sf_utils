4d28b2807d78ea922cfe7282067d1a7a607055d7
maguirre <maguirre@optimeconsulting.com>
2023-03-22 15:34:02 -0500

    Configurando auto refresh de traduccion

--- src/Serializer/Normalizer/TranslatableContentNormalizer.php
+++ src/Serializer/Normalizer/TranslatableContentNormalizer.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Optime\Util\Serializer\Normalizer;
 
+use Optime\Util\Translation\DefaultLocaleEntityRefresh;
 use Optime\Util\Translation\TranslatableContent;
 use Optime\Util\Translation\TranslatableContentFactory;
 use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
@@ -16,8 +17,12 @@ use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
  */
 class TranslatableContentNormalizer implements ContextAwareNormalizerInterface
 {
-    public function __construct(private TranslatableContentFactory $contentFactory)
-    {
+    const AUTO_REFRESH = 'auto_refresh_locale';
+
+    public function __construct(
+        private readonly TranslatableContentFactory $contentFactory,
+        private readonly DefaultLocaleEntityRefresh $entityRefresh,
+    ) {
     }
 
     public function supportsNormalization($data, string $format = null, array $context = []): bool
@@ -30,6 +35,11 @@ class TranslatableContentNormalizer implements ContextAwareNormalizerInterface
         /** @var TranslatableContent $object */
 
         if ($object->isPending()) {
+
+            if ($context[self::AUTO_REFRESH] ?? false) {
+                $this->entityRefresh->refresh($object->getTarget());
+            }
+
             $object = $this->contentFactory->load(
                 $object->getTarget(),
                 $object->getProperty()
